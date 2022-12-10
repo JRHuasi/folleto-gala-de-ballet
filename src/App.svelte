91acf05f949da41bd5fb88280357a37f65578ac1
jrhuasi <julianra@gmail.com>
2022-12-10 15:45:50 -0300

    presentación

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Router } from "@roxi/routify";
+  import { routes } from "../.routify/routes";
+  import GlobalScssImports from "./GlobalScssImports.svelte";
+
+</script>
+
+<GlobalScssImports />
+
+<Router {routes} />
---
+++ src/pages/index.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { metatags } from '@roxi/routify'
+	import {fade} from 'svelte/transition'
+
+	import Fa from 'svelte-fa';
+	import { faTimes, faArrowLeft, faArrowRight, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+	metatags.title = 'GALA DE BALLET 2022'
+	metatags.description = 'Claudia Ayusa - Estudio de Danza Clásica'
+
+	const carouselPhotos = [
+		'/imagenes/imagen_0.jpg',
+		'/imagenes/imagen_1.jpg',
+		'/imagenes/imagen_2.jpg',
+		'/imagenes/imagen_3.jpg',
+		'/imagenes/imagen_4.jpg',
+		'/imagenes/imagen_5.jpg',
+		'/imagenes/imagen_6.jpg',
+		'/imagenes/imagen_7.jpg',
+		'/imagenes/imagen_8.jpg',
+		'/imagenes/imagen_9.jpg',
+		'/imagenes/imagen_a.jpg',
+		'/imagenes/imagen_b.jpg',
+		'/imagenes/imagen_c.jpg',
+	]
+
+  let index = 0
+	
+	const next = () => {
+		index = (index + 1) % carouselPhotos.length
+	}
+
+	const prev = () => {
+    index = (index - 1 + carouselPhotos.length) % carouselPhotos.length
+	}
+
+	$: console.log({index})
+
+</script>
+
+<div class="cuadro">
+	{#each [carouselPhotos[index]] as src (index)}
+		<img transition:fade {src} alt="" />	
+	{/each}
+	<div id="cursores">
+		<div class="flecha izquierda" on:click={prev}>
+			<Fa icon={faAngleLeft}/>
+		</div>
+		<div class="flecha derecha" on:click={next}>
+			<Fa icon={faAngleRight}/>
+		</div>
+	</div>
+</div>
